0617f3428dd3e503e562fbe59904a165656e93e7
dperille <dannyperille@gmail.com>
2025-08-12 23:29:55 -0600

    highlight yanked

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -9,3 +9,12 @@
     -- end,
 -- })
 
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank({
+            timeout = 200,
+        })
+    end,
+})
+
