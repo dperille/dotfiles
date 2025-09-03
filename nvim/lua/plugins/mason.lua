0b6c4f60a949b8384819bf157c2802536c831929
Daniel Perille <dannyperille@gmail.com>
2025-09-03 12:45:43 -0600

    housekeeping

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,5 @@
 -- Used to pre-install external editing tooling rather than needing to do so manually
--- Individual config for LSPs in lsp-config, linters/formatters in none-ls
+-- Individual config for LSPs in lsp-config, linters/formatters in null-ls
 return {
     {
         "williamboman/mason.nvim",
