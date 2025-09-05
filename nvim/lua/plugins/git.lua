f360b56c046251808c2a4c7d29edd412cc4a2764
Daniel Perille <dannyperille@gmail.com>
2025-09-04 20:33:08 -0600

    remove fugitive, add gitsigns/telescope git keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,15 +1,11 @@
 return {
     {
-        "tpope/vim-fugitive",
-        config = function()
-            -- <Space> + g + b to git blame
-            vim.keymap.set("n", "<leader>gb", ":Git blame<cr>", { desc = "[G]it [B]lame" })
-        end,
-    },
-    {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require("gitsigns").setup({
+            local gitsigns = require("gitsigns")
+
+            gitsigns.setup({
+                word_diff = false, -- Whether to highglight individual changes within line
                 current_line_blame = true,
                 current_line_blame_opts = {
                     virt_text = true,
@@ -21,6 +17,26 @@ return {
                 update_debounce = 100,
                 max_file_length = 40000,
             })
+
+            -- Gitsigns
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", { desc = "[G]it [P]review Hunk" })
+            vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>", { desc = "[G]it [U]ndo Hunk" })
+            vim.keymap.set("n", "]g",
+                function()
+                    gitsigns.nav_hunk("next", { wrap = true, preview = true, greedy = false })
+                end,
+                { desc = "[G]it [N]ext Hunk" })
+            vim.keymap.set("n", "[g",
+                function()
+                    gitsigns.nav_hunk("prev", { wrap = true, preview = true, greedy = false })
+                end,
+                { desc = "[G]it [N]ext Hunk" })
+
+            -- Telescope git commands
+            vim.keymap.set("n", "<leader>gs", ":Telescope git_status<CR>", { desc = "[G]it [S]tatus" })     -- <Tab> to stage/unstage
+            vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { desc = "[G]it [B]ranches" }) -- <CR> to checkout, <C-y> to merge
+            vim.keymap.set("n", "<leader>gm", ":Telescope git_bcommits<CR>",
+                { desc = "[G]it co[M]mits for current buffer" })
         end,
     },
 }
