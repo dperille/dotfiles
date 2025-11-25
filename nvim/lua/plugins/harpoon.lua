57b12ce2abe95b2270fae65d6897cec74172e68d
Daniel Perille <dannyperille@gmail.com>
2025-11-25 12:45:06 -0600

    fix harpoon window width invalid float

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
         require("harpoon").setup({
             tabline = true,
             menu = {
-                width = vim.api.nvim_win_get_width(0) / 2,
+                width = math.floor(vim.api.nvim_win_get_width(0) / 2),
             }
         })
 
