a25d5d5e112a9d82420a3f2947ecf8a3d885c87a
dperille <dannyperille@gmail.com>
2026-04-16 19:44:42 +1000

    add autotag

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,18 +1,30 @@
--- Auto-close quotes/braces/etc
 return {
-    "windwp/nvim-autopairs",
-    event = { "InsertEnter" },
-    dependencies = {
-        "hrsh7th/nvim-cmp",
+    -- Auto-close quotes/braces/etc
+    {
+        "windwp/nvim-autopairs",
+        event = { "InsertEnter" },
+        config = function()
+            require("nvim-autopairs").setup({
+                check_ts = true,
+                ts_config = {
+                    lua = { "string" },
+                    javascript = { "template_string" },
+                    java = false,
+                },
+            })
+        end,
     },
-    config = function()
-        require("nvim-autopairs").setup({
-            check_ts = true,
-            ts_config = {
-                lua = { "string" },
-                javascript = { "template_string" },
-                java = false,
-            },
-        })
-    end,
+    -- Auto-close html tags
+    {
+        "windwp/nvim-ts-autotag",
+        event = { "InsertEnter" },
+        config = function()
+            require("nvim-ts-autotag").setup({
+                opts = {
+                    enable_close = true, -- Auto-close tags
+                    enable_rename = true, -- If renaming one tag, auto-renames matching one
+                },
+            })
+        end,
+    }
 }
