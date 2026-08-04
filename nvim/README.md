9c6a7b5fa542c049bb940748d6bf50b4b58eb0e1
dperille <dannyperille@gmail.com>
2026-08-03 19:05:16 -0600

    fix treesitter <-> nvim 0.12.0 break

--- nvim/README.md
+++ nvim/README.md
@@ -7,3 +7,4 @@
         * In ITerm2: Settings > Profiles > Text > Font (and also set the vertical spacing to 115)
 * Ripgrep
 * Node & npm (for ts_ls)
+* Treesitter - as of Neovim 0.12.0, requires CLI (`brew install tree-sitter-cli`) and a C compiler on your path (eg clang)
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,35 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    -- Update installed Tree-sitter parsers whenever nvim-treesitter is installed or updated
-    build = ":TSUpdate",
+    lazy = false,
+    branch = 'main',
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "vim",
-                "vimdoc",
-                "lua",
-                "java",
-                "go",
-                "gomod",
-                "gowork",
-                "python",
-                "javascript",
-                "typescript",
-                "html",
-                "css",
-                "json",
-                "tsx",
-                "markdown",
-                "markdown_inline",
-                "gitignore",
-            },
-            highlight = {
-                enable = true,
-            },
+        require("nvim-treesitter").install({
+            "vim",
+            "vimdoc",
+            "lua",
+            "java",
+            "go",
+            "gomod",
+            "gowork",
+            "python",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "json",
+            "tsx",
+            "markdown",
+            "markdown_inline",
+            "gitignore",
+        })
+        
+        -- Enable treesitter highlighting
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+            end,
         })
     end,
+    -- Update installed Tree-sitter parsers whenever nvim-treesitter is installed or updated
+    build = ":TSUpdate",
 }
